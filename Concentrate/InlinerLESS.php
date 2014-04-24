9984157fdbbf2111e30854c916209b69151326ed
Michael Gauthier <mike@silverorange.com>
2014-04-24 15:14:47 -0300

    Don't try to inline paths containing interplorated LESS variables.

--- Concentrate/InlinerLESS.php
+++ Concentrate/InlinerLESS.php
@@ -6,11 +6,31 @@ require_once 'Concentrate/InlinerCSS.php';
  * @category  Tools
  * @package   Concentrate
  * @author    Michael Gauthier <mike@silverorange.com>
- * @copyright 2013 silverorange
+ * @copyright 2013-2014 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class Concentrate_InlinerLESS extends Concentrate_InlinerCSS
 {
+	protected function inlineImportCallback($matches)
+	{
+		$replacement = '';
+
+		if (isset($matches[2])) {
+			$uri = trim($matches[2], '\'"');
+		} else {
+			$uri = trim($matches[1], '\'"');
+		}
+
+		// check if it contains an interplorated LESS variable
+		$uriMatches = array();
+		if (preg_match('/@{[\w-]+}/', $uri, $uriMatches) === 1) {
+			$replacement = $matches[0];
+		} else {
+			$replacement = parent::inlineImportCallback($matches);
+		}
+
+		return $replacement;
+	}
 }
 
 ?>

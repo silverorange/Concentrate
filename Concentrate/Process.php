1f4f018e842bf2fff481763fd19e80466c46195d
Michael Gauthier <mike@silverorange.com>
2022-01-06 21:17:49 -0400

    Stop using shell_exec because it corrupts both input and output

---
+++ Concentrate/Process.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @category  Tools
+ * @package   Concentrate
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2022 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class Concentrate_Process
+{
+    const FD_STDIN = 0;
+    const FD_STDOUT = 1;
+    const FD_STDERR = 2;
+
+    protected $command = '';
+
+    public function __construct(string $command)
+    {
+        $this->command = $command;
+    }
+
+    public function run(string $input): string
+    {
+        $descriptorSpec = array(
+            self::FD_STDIN => ['pipe', 'r'],
+            self::FD_STDOUT => ['pipe', 'w'],
+            self::FD_STDERR => ['pipe', 'w'],
+        );
+
+        $pipes = [];
+        $process = proc_open($this->command, $descriptorSpec, $pipes);
+
+        if (!is_resource($process)) {
+            throw new Concentrate_ProcessException(
+                'Unable to open process for "' . $this->command . '".',
+                0
+            );
+        }
+
+        fwrite($pipes[self::FD_STDIN], $input);
+        fclose($pipes[self::FD_STDIN]);
+
+        $output = stream_get_contents($pipes[self::FD_STDOUT]);
+        fclose($pipes[self::FD_STDOUT]);
+
+        $error = stream_get_contents($pipes[self::FD_STDERR]);
+        fclose($pipes[self::FD_STDERR]);
+
+        $returnValue = proc_close($process);
+
+        if ($returnValue !== 0) {
+            throw new Concentrate_ProcessException(
+                'Error running "' . $this->command . '": ' . $error,
+                0
+            );
+        }
+
+        return $output;
+    }
+}
+
+?>
---
+++ Concentrate/ProcessException.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @category  Tools
+ * @package   Concentrate
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2022 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class Concentrate_ProcessException extends Exception
+{
+}
+
+?>
